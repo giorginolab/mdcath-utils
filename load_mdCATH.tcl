b180ce19498c27eaa878b5ea9518bd6f8a83051d
Toni G <tonigi@users.noreply.github.com>
2024-04-09 17:20:54 +0200

    Update load_mdCATH.tcl

--- load_mdCATH.tcl
+++ load_mdCATH.tcl
@@ -1,22 +1,48 @@
 proc load_mdCATH {fn temperature replica} { 
-	set tmp [exec h5ls $fn]
-	set code [lindex $tmp 0]
-	set tmpdir $::env(TMPDIR)
-	set pdbname $tmpdir/loadmdcath.pdb 
-	exec h5dump -b -o $pdbname -d /$code/pdbProteinAtoms $fn
-	set cbin $tmpdir/coords.bin
-	exec h5dump -b -o $cbin -d /$code/sims${temperature}K/$replica/coords $fn
+    # Try to execute h5ls and handle errors
+    set status [catch {exec h5ls $fn} tmp]
+    if {$status} {
+        return -code error "Error executing h5ls on file $fn: $tmp"
+    }
+    set code [lindex $tmp 0]
 
-	mol new $pdbname
-	set N [molinfo top get numatoms]
-	animate delete all
+    set tmpdir $::env(TMPDIR)
+    set pdbname $tmpdir/loadmdcath.pdb 
 
-	set fp [open $cbin r]
-	fconfigure $fp -translation binary
-	set cdat [read $fp]
-	close $fp
+    # Handle potential errors from h5dump
+    if {[catch {exec h5dump -b -o $pdbname -d /$code/pdbProteinAtoms $fn} result]} {
+        return -code error "Error dumping pdbProteinAtoms from $fn: $result"
+    }
 
-	set M [binary scan $cdat f* dat]
+    set cbin $tmpdir/coords.bin
+    if {[catch {exec h5dump -b -o $cbin -d /$code/sims${temperature}K/$replica/coords $fn} result]} {
+        return -code error "Error dumping coords from $fn: $result"
+    }
 
-	return $dat
+    # Load the molecular data
+    mol new $pdbname
+    set N [molinfo top get numatoms]
+    if {$N == 0} {
+        return -code error "No atoms found in the molecule loaded from $pdbname"
+    }
+    animate delete all
+
+    # Handle file opening and binary data reading
+    if {[catch {open $cbin r} fp msg]} {
+        return -code error "Error opening coordinates file $cbin: $msg"
+    }
+    fconfigure $fp -translation binary
+    if {[catch {read $fp} cdat]} {
+        close $fp
+        return -code error "Error reading data from coordinates file $cbin"
+    }
+    close $fp
+
+    # Binary data processing
+    set M [binary scan $cdat f* dat]
+    if {$M == 0} {
+        return -code error "Failed to scan binary data from $cbin"
+    }
+
+    return $dat
 }
